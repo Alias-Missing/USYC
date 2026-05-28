e0deb8710b97ffbe4bbaec751782149f0b7c9e86
alias-missing <aandw.rodriguez@gmail.com>
2026-05-28 17:38:50 -0500

    Consolidate PrintMenu Method 2/3

--- USYC-Polyphonic/PrintMenu.java
+++ USYC-Polyphonic/PrintMenu.java
@@ -32,4 +32,11 @@ public class PrintMenu
     RandomSplashText.RandomSpalshText();
     System.out.println("Today is: " + LocalDate.now());
 }
+    public static void PrintMen_FMA()
+{
+    System.out.println("----USYC > PHYSICS > FMA > ----");
+    System.out.println("1. Solve for Force");
+    System.out.println("2. Solve for Mass");
+    System.out.println("3. Solve for Accleration");
+}
 }
