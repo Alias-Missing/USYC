89a94cc55088b73b6da887fa09193cc06e89142f
alias-missing <aandw.rodriguez@gmail.com>
2026-05-28 17:39:30 -0500

    Consolidate PrintMenu 3/3

--- USYC-Polyphonic/PrintMenu.java
+++ USYC-Polyphonic/PrintMenu.java
@@ -39,4 +39,14 @@ public class PrintMenu
     System.out.println("2. Solve for Mass");
     System.out.println("3. Solve for Accleration");
 }
+         public static void PrintMen_GASLAW()
+{
+    System.out.println("----USYC > PHYSICS > GAS LAWS > ----");
+    System.out.println("1. Solve for Pressure 1");
+    System.out.println("2. Solve for Pressure 2");
+    System.out.println("3. Solve for Volume 1");
+    System.out.println("4. Solve for Volume 2");
+    System.out.println("5. Solve for Temperature 1");
+    System.out.println("6. Solve for Temperature 2");
+}
 }
