cd2e82677625738974d66ed9c2e62938b7287aaa
alias-missing <aandw.rodriguez@gmail.com>
2026-05-28 17:57:18 -0500

    Consolidating GetMenuChoice 2/3

--- USYC-Polyphonic/GetMenuChoice.java
+++ USYC-Polyphonic/GetMenuChoice.java
@@ -17,4 +17,21 @@ public class GetMenuChoice
     while (opt<1||opt>17);
     return opt;
 }
+    public class GetMenuChoice_FMA 
+{
+    public static int GetMenuOpt_FMA(Scanner kybrd)
+{
+    int opt;
+    do
+    {
+        System.out.print("Please enter a menu option: ");
+        opt= kybrd.nextInt();
+        if (opt>3||opt<1)
+        {
+            System.out.println("Error: Invalid Menu option: Please try again!");
+        }
+    }
+    while (opt<1||opt>3);
+    return opt;
+}
 }
