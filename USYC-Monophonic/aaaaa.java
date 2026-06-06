4da8e0375c706b79885ccfc578bc86d8fb4b9b33
Alias-Missing <aandw.rodriguez@gmail.com>
2026-06-06 01:36:43 -0500

    Catchup Monophonic to Polyphonic, 1/2

--- USYC-Monophonic/aaaaa.java
+++ USYC-Monophonic/aaaaa.java
@@ -1,10 +1,145 @@
-import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.MonthDay;
+import java.util.Scanner;
 
 public class aaaaa
 {
+    static final double MolarMassArray[] = new double[120];
+    static {
+        MolarMassArray[0]=1.0078; //H
+        MolarMassArray[1]=4.0026; //He
+        MolarMassArray[2]=6.9410; //Li
+        MolarMassArray[3]=9.0122; //Be
+        MolarMassArray[4]=10.811; //B
+        MolarMassArray[5]=12.011; //C
+        MolarMassArray[6]=14.007; //N
+        MolarMassArray[7]=15.999; //O
+        MolarMassArray[8]=18.998; //F
+        MolarMassArray[9]=20.180; //Ne
+        MolarMassArray[10]=22.990; //Na
+        MolarMassArray[11]=24.305; //Mg
+        MolarMassArray[12]=26.982; //Al
+        MolarMassArray[13]=28.086; //Si
+        MolarMassArray[14]=30.974; //P
+        MolarMassArray[15]=32.065; //S
+        MolarMassArray[16]=35.453; //Cl
+        MolarMassArray[17]=39.948; //Ar
+        MolarMassArray[18]=39.098; //K
+        MolarMassArray[19]=40.078; //Ca
+        MolarMassArray[20]=44.956; //Sc
+        MolarMassArray[21]=47.867; //Ti
+        MolarMassArray[22]=50.942; //V
+        MolarMassArray[23]=51.996; //Cr
+        MolarMassArray[24]=54.938; //Mn
+        MolarMassArray[25]=55.845; //Fe
+        MolarMassArray[26]=58.933; //Co
+        MolarMassArray[27]=58.693; //Ni
+        MolarMassArray[28]=63.546; //Cu
+        MolarMassArray[29]=65.380; //Zn
+        MolarMassArray[30]=69.723; //Ga
+        MolarMassArray[31]=72.640; //Ge
+        MolarMassArray[32]=74.922; //As
+        MolarMassArray[33]=78.960; //Se
+        MolarMassArray[34]=79.904; //Br
+        MolarMassArray[35]=83.798; //Kr
+        MolarMassArray[36]=85.468; //Rb
+        MolarMassArray[37]=87.620; //Sr
+        MolarMassArray[38]=88.906; //Y
+        MolarMassArray[39]=91.224; //Zr
+        MolarMassArray[40]=92.906; //Nb
+        MolarMassArray[41]=95.950; //Mo
+        MolarMassArray[42]=98.000; //Tc
+        MolarMassArray[43]=101.07; //Ru
+        MolarMassArray[44]=102.91; //Rh
+        MolarMassArray[45]=106.42; //Pd
+        MolarMassArray[46]=107.87; //Ag
+        MolarMassArray[47]=112.41; //Cd
+        MolarMassArray[48]=114.82; //In
+        MolarMassArray[49]=118.71; //Sn
+        MolarMassArray[50]=121.76; //Sb
+        MolarMassArray[51]=127.60; //Te
+        MolarMassArray[52]=126.90; //I
+        MolarMassArray[53]=131.29; //Xe
+        MolarMassArray[54]=132.91; //Cs
+        MolarMassArray[55]=137.33; //Ba
+        MolarMassArray[56]=138.91; //La
+        MolarMassArray[57]=140.12; //Ce
+        MolarMassArray[58]=140.91; //Pr
+        MolarMassArray[59]=144.24; //Nd
+        MolarMassArray[60]=145.00; //Pm
+        MolarMassArray[61]=150.36; //Sm
+        MolarMassArray[62]=151.96; //Eu
+        MolarMassArray[63]=157.25; //Gd
+        MolarMassArray[64]=158.93; //Tb
+        MolarMassArray[65]=162.50; //Dy
+        MolarMassArray[66]=164.93; //Ho
+        MolarMassArray[67]=167.26; //Er
+        MolarMassArray[68]=168.93;
+        MolarMassArray[69]=173.04;
+        MolarMassArray[70]=174.97;
+        MolarMassArray[71]=178.49;
+        MolarMassArray[72]=180.95;
+        MolarMassArray[73]=183.84;
+        MolarMassArray[74]=186.21;
+        MolarMassArray[75]=190.23;
+        MolarMassArray[76]=192.22;
+        MolarMassArray[77]=195.08;
+        MolarMassArray[78]=196.97;
+        MolarMassArray[79]=200.59;
+        MolarMassArray[80]=204.38;
+        MolarMassArray[81]=207.20;
+        MolarMassArray[82]=208.98;
+        MolarMassArray[83]=209.00;
+        MolarMassArray[84]=210.00;
+        MolarMassArray[85]=222.00;
+        MolarMassArray[86]=223.00;
+        MolarMassArray[87]=226.00;
+        MolarMassArray[88]=227.00;
+        MolarMassArray[89]=232.04;
+        MolarMassArray[90]=231.04;
+        MolarMassArray[91]=238.03;
+        MolarMassArray[92]=237.05;
+        MolarMassArray[93]=244.00;
+        MolarMassArray[94]=243.00;
+        MolarMassArray[95]=247.00;
+        MolarMassArray[96]=247.00;
+        MolarMassArray[97]=251.00;
+        MolarMassArray[98]=252.00;
+        MolarMassArray[99]=257.00;
+        MolarMassArray[100]=258.00;
+        MolarMassArray[101]=259.00;
+        MolarMassArray[102]=262.00;
+        MolarMassArray[103]=267.00;
+        MolarMassArray[104]=262.00;
+        MolarMassArray[105]=269.00;
+        MolarMassArray[106]=264.00;
+        MolarMassArray[107]=269.00;
+        MolarMassArray[108]=278.00;
+        MolarMassArray[109]=281.00;
+        MolarMassArray[110]=282.00;
+        MolarMassArray[111]=285.00;
+        MolarMassArray[112]=286.00;
+        MolarMassArray[113]=289.00;
+        MolarMassArray[114]=289.00;
+        MolarMassArray[115]=293.00;
+        MolarMassArray[116]=294.00;
+        MolarMassArray[117]=294.00;
+            }
+    public static double GetMass(Scanner kybrd)
+    {
+        int atomnum;
+        do
+        {
+            System.out.print("Enter the atomic number of the element you want, 1-118: ");
+            atomnum= kybrd.nextInt();
+            if (atomnum<1||atomnum>118)
+            {
+                System.out.println("Invalid Atomic Number as of 2026-June-04");
+            }
+        } while(atomnum<1||atomnum>118);
+        return MolarMassArray[atomnum-1];
+    }
     public static void RandomSplashText()
 {
     LocalDate today= LocalDate.now();
@@ -13,99 +148,65 @@ public class aaaaa
     int SpalshText=(int)(Math.random()*23)+1;
     switch (SpalshText)
     {
-    case 1:
-        {
+    case 1 -> {
             System.out.println("Nine Sols Characters: *Exists* Me: Would");
         }
-    break;
-    case 2:
-        {
+    case 2 -> {
         System.out.println("TREACHERY TOMORROW!!");
         }
-    break;
-    case 3:
-        {
+    case 3 -> {
         System.out.println("I WANT TO BELIEVE.");
         System.out.println("SIZE 2 FISH.");
         }
 
-    break;
-
-    case 4:
-        {
+    case 4 -> {
         System.out.println("MANKIND IS DEAD");
         System.out.println("BLOOD IS FUEL");
         System.out.println("HELL IS FULL");
         }
-    break;
-    case 5:
-        {
+    case 5 -> {
             System.out.println("How do people make games out of Java?");
         }
-    break;
-    case 6:
-        {
+    case 6 -> {
             System.out.println("Man, I need some kind of app development guy on this shit right fucking now!");
         }
-    break;
-    case 7:
-        {
+    case 7 -> {
             System.out.println("THY END IS NOW!");
             System.out.println("CRUSH!");
             System.out.println("DIE!");
             System.out.println("JUDGEMENT!");
         }
-        break;
-        case 8:
-        {
+        case 8 -> {
             System.out.println("Will anyone read this? Probably not.");
         }
-        break;
 
-        case 9:
-        {    
+        case 9 -> {    
             System.out.println("YOMI HUSTLE!");
         }
-       break;
-        case 10:
-        {    
+        case 10 -> {    
             System.out.println("This is (no longer) longer than main lmfao. Fact-Checked by: The Fucking Calculator That Came On The Fucking Laptop (TM)");
         }
-        break;
-        case 11:
-        {
+        case 11 -> {
             System.out.println("Don't look at bbbbb.java, it's THE MONOLITH (TM)");
         }
-        break;
-        case 12:
-        {
+        case 12 -> {
             System.out.println("bbbbb 325 lines? Nah.");
             System.out.println("aaaaa *1373 lines? Yea.");
         }
-        break;
-        case 13:
-        {
+        case 13 -> {
             System.out.println("Stand proud, bbbbb.java, you were strong.");
         }
-        break;
-        case 14:
-        {
+        case 14 -> {
             System.out.println("Nah, I'd aaaaa.java");
         }
-        break;
-        case 15:
-        {
+        case 15 -> {
             System.out.println("Hours spent making main menu: 0.00003");
             System.out.println("Hours spent making these splash texts: 333333.333333");
         }
-        break;
-        case 16:
-        {
+        case 16 -> {
             System.out.println("Still smaller than the Oregon Trail! 34 KB<3500 km");
         }
-        break;
-        case 17:
-        {
+        case 17 -> {
             System.out.println("Geforce RTX 5060");
             System.out.println("B860M D3HP");
             System.out.println("Peerless Assassin 120 SE");
@@ -114,39 +215,26 @@ public class aaaaa
             System.out.println("MSI PSU");
             System.out.println("Dev Specs");
         }
-        break;
-        case 18:
-        {
+        case 18 -> {
             System.out.println("I am going to ULTRAKILL you!!");
         }
-        break;
-        case 19:
-        {
+        case 19 -> {
             System.out.println("Guest");
             System.out.println("Visit");
         }
-        break;
-        case 20:
-        {
+        case 20 -> {
             System.out.println("Also try Bet Break!");
             System.out.println("(Note: Bet Break is not availible to the public yet, and, so, you cannot, in fact, try Bet Break.)");
         }
-        break;
-        case 21:
-        {
+        case 21 -> {
             System.out.println("Also see: LinuxGuide! (I think I accidentally wiped it off my computer from installing ArchLinux...)");
         }
-        break;
-        case 22:
-        {
+        case 22 -> {
             System.out.println("If any potential employer sees this, I hope you're prepared for my only capability: massive CLI projects. :D");
         }
-        break;
-        case 23:
-        {
+        case 23 -> {
             System.out.println("You wanna guess how much time PgUp and PgDwn save scrolling through this thing?");
         }
-        break;
     }
     if (now.equals(crimas))
     {
@@ -353,13 +441,14 @@ public class aaaaa
     System.out.println("10. Mol-to-Atom");
     System.out.println("11. Stoichiometry- Mol-to-Mol");
     System.out.println("12. Stoichiometry- Mol-to-Gram");
+    System.out.println("13. Molar Mass Lookup");
     System.out.println("----BASIC----");
-    System.out.println("13. Convert Fahrenheit to Celsius");
-    System.out.println("14. Convert Celsius to Fahrenheit");
-    System.out.println("15. Secret Menu");
+    System.out.println("14. Convert Fahrenheit to Celsius");
+    System.out.println("15. Convert Celsius to Fahrenheit");
+    System.out.println("16. Secret Menu");
     System.out.println("----PHYSICS----");
-    System.out.println("16. Force, Mass, and Acceleration");
-    System.out.println("17. Gas Laws");
+    System.out.println("17. Force, Mass, and Acceleration");
+    System.out.println("18. Gas Laws");
     System.out.println("Planned Updates: Uhhhhh");
     System.out.println("Latest Update: Physics Section, Gas Laws, more spalsh techs, Consistent Code Alignment");
     RandomSplashText();
@@ -539,8 +628,8 @@ public class aaaaa
 }
     public static void main(String[] args)
 {
-    String Confirm=" ";
-    double distance, area_circle, area_triangle, area_quadril, angle_sum, mole, atom, gram, Celsius, Fahrenheit;
+    String Confirm="";
+    double distance, area_circle, area_triangle, area_quadril, angle_sum, mole, atom, gram, Celsius, Fahrenheit, molar_mass;
     Scanner kybrd=new Scanner(System.in);
     do
     {
@@ -642,8 +731,15 @@ public class aaaaa
             System.out.print("Would you like to make another selection? ");
             Confirm=kybrd.next();
         }
-        break;
         case 13:
+        {
+            molar_mass=GetMass(kybrd);
+            System.out.println("Molar Mass: " + molar_mass);
+            System.out.print("Would you like to make another selection? ");
+            Confirm=kybrd.next();
+        }
+        break;
+        case 14:
         {
             Celsius=FtoC(kybrd);
             System.out.println("Temp. in Celsius: " + Celsius);
@@ -651,7 +747,7 @@ public class aaaaa
             Confirm=kybrd.next();
         }
         break;
-        case 14:
+        case 15:
         {
             Fahrenheit = CtoF(kybrd);
             System.out.println("Temp. in Fahrenheit: " + Fahrenheit);
@@ -659,7 +755,7 @@ public class aaaaa
             Confirm=kybrd.next();
         }
         break;
-        case 15:
+        case 16:
         {
             String[] user_expected={"Guest", "DIRECTIVE", "DEV"};
             String[] pass_expected={"Visit", "PERM.THETA.01", "LIST"};
@@ -690,85 +786,67 @@ public class aaaaa
             }  
         }
         break;
-                    case 16:
+                    case 17:
                         {
                             PrintMenu_FMA();
                             choice = GetMenuChoice_FMA(kybrd);
                             switch(choice)
                             {
-                                case 1:
-                                    {
+                                case 1 -> {
                                         SolveForForce(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                    break;
-                                case 2:
-                                    {
+                                case 2 -> {
                                         SolveForMass(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                    break;
-                                case 3:
-                                    {
+                                case 3 -> {
                                         SolveForAccel(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                    break;
                             }
                         }
                     break;
-                    case 17:
+                    case 18:
                         {
                             PrintMenu_GASLAW();
 
                             choice = GetMenuChoice_GASLAW(kybrd);
                             switch (choice)
                             {
-                                case 1:
-                                    {
+                                case 1 -> {
                                        SolveForPressure1(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                break;
-                                case 2:
-                                    {
+                                case 2 -> {
                                         SolveForPressure2(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                break;
-                                case 3:
-                                    {
+                                case 3 -> {
                                        SolveForVol1(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next(); 
                                     }
-                                break;
-                                case 4:
-                                    {
+                                case 4 -> {
                                         SolveForVol2(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                break;
-                                case 5:
-                                    {
+                                case 5 -> {
                                         SolveForTemp1(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                break;
-                                case 6:
-                                    {
+                                case 6 -> {
                                         SolveForTemp2(kybrd);
                                         System.out.print("Would you like to make another selection? ");
                                         Confirm=kybrd.next();
                                     }
-                                break;
                             }
                         }
         }
